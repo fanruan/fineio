4b5cca38b4b35ce8a48426a42a7e555c35caa117
Yee <yee@zyee.me>
2018-09-03 14:00:07 +0800

    Append调整

--- src/main/java/com/fineio/io/file/AppendIOFile.java
+++ src/main/java/com/fineio/io/file/AppendIOFile.java
@@ -11,7 +11,6 @@ import com.fineio.io.FloatBuffer;
 import com.fineio.io.IntBuffer;
 import com.fineio.io.LongBuffer;
 import com.fineio.io.ShortBuffer;
-import com.fineio.io.read.ReadOnlyBuffer;
 import com.fineio.io.write.WriteOnlyBuffer;
 import com.fineio.storage.Connector;
 
@@ -48,8 +47,7 @@ public class AppendIOFile<T extends Buffer> extends AbstractReadIOFile<T> {
             throw new UnsupportedOperationException(String.format("Index %d is unsupported in Append MODE"));
         }
         if (index == maxBlockIndex) {
-            ReadOnlyBuffer buffer = model.createBufferForRead(connector, createIndexBlock(index), block_size_offset);
-            buffer.flip();
+            return model.createBufferForEdit(connector, createIndexBlock(index), block_size_offset);
         }
         return model.createBufferForWrite(connector, createIndexBlock(index), block_size_offset);
     }
