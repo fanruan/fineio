c01f64f0a4a9cc69958e30226b92fb89307c4110
Yee <yee@fanruan.com>
2018-11-20 23:43:31 +0800

    doWhenFinish重做 加个定时任务check 任务队列是否还有未执行的

--- src/main/java/com/fineio/io/file/writer/JobFinishedManager.java
+++ src/main/java/com/fineio/io/file/writer/JobFinishedManager.java
@@ -9,6 +9,7 @@ import com.fineio.logger.FineIOLoggers;
 import com.fineio.thread.FineIOExecutors;
 
 import java.net.URI;
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -16,6 +17,8 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yee
@@ -25,8 +28,9 @@ public final class JobFinishedManager {
     private ExecutorCompletionService<Job> service =
             new ExecutorCompletionService<Job>(FineIOExecutors.newSingleThreadExecutor("JobFinishedManager-complete"));
     private ExecutorService consume = FineIOExecutors.newSingleThreadExecutor(JobFinishedManager.class);
+    private final Queue<Pair<TaskKey, Object>> queue = new ConcurrentLinkedQueue<Pair<TaskKey, Object>>();
     private volatile static JobFinishedManager instance;
-    private Queue<Pair<TaskKey, Object>> queue = new ConcurrentLinkedQueue<Pair<TaskKey, Object>>();
+    private ScheduledExecutorService timer = FineIOExecutors.newScheduledExecutorService(1, "JobFinishedManager-timer");
 
 
     public static JobFinishedManager getInstance() {
@@ -47,22 +51,20 @@ public final class JobFinishedManager {
                 while (true) {
                     try {
                         service.take().get().doJob();
-                        Pair<TaskKey, Object> pair = null;
-                        while ((pair = queue.peek()) != null) {
-                            if (pair.getKey().getType().equals(TaskKey.KeyType.DONE)) {
-                                queue.remove(pair);
-                                ((Runnable) pair.getValue()).run();
-                                FineIOLoggers.getLogger().debug("Run finish Task");
-                            } else {
-                                break;
-                            }
-                        }
                     } catch (Exception e) {
                         FineIOLoggers.getLogger().error(e);
                     }
                 }
             }
         });
+        timer.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                if (!queue.isEmpty()) {
+                    submit(URI.create("JobFinishedManager_timer_checker"));
+                }
+            }
+        }, 10, 10, TimeUnit.SECONDS);
     }
 
     void submit(final URI uri) {
@@ -72,15 +74,17 @@ public final class JobFinishedManager {
                 return new Job() {
                     @Override
                     public void doJob() {
-                        Pair<TaskKey, Object> pair = null;
-                        while ((pair = queue.peek()) != null) {
-                            if (pair.getKey().getType().equals(TaskKey.KeyType.DONE)) {
-                                queue.remove(pair);
-                                ((Runnable) pair.getValue()).run();
-                                FineIOLoggers.getLogger().debug("Run finish Task");
-                            } else if (uri.equals(pair.getValue())) {
-                                queue.remove(pair);
-                                break;
+                        synchronized (queue) {
+                            Iterator<Pair<TaskKey, Object>> iterator = queue.iterator();
+                            while (iterator.hasNext()) {
+                                Pair<TaskKey, Object> pair = iterator.next();
+                                if (pair.getKey().getType().equals(TaskKey.KeyType.DONE)) {
+                                    iterator.remove();
+                                    ((Runnable) pair.getValue()).run();
+                                    FineIOLoggers.getLogger().debug("Run finish Task");
+                                } else if (uri.equals(pair.getValue())) {
+                                    iterator.remove();
+                                }
                             }
                         }
                     }
@@ -90,19 +94,25 @@ public final class JobFinishedManager {
     }
 
     public void addTask(URI uri) {
-        queue.offer(new Pair<TaskKey, Object>(new FinishOneTaskKey(uri), uri));
+        synchronized (queue) {
+            queue.offer(new Pair<TaskKey, Object>(new FinishOneTaskKey(uri), uri));
+        }
     }
 
     public <T> Future<T> finish(Callable<T> runnable) {
-        FutureTask<T> task = new FutureTask<T>(runnable);
-        queue.offer(new Pair<TaskKey, Object>(new DoneTaskKey(), task));
-        return task;
+        synchronized (queue) {
+            FutureTask<T> task = new FutureTask<T>(runnable);
+            queue.offer(new Pair<TaskKey, Object>(new DoneTaskKey(), task));
+            return task;
+        }
     }
 
     public Future<Void> finish(Runnable runnable) {
-        FutureTask<Void> task = new FutureTask<Void>(runnable, null);
-        queue.offer(new Pair<TaskKey, Object>(new DoneTaskKey(), task));
-        return task;
+        synchronized (queue) {
+            FutureTask<Void> task = new FutureTask<Void>(runnable, null);
+            queue.offer(new Pair<TaskKey, Object>(new DoneTaskKey(), task));
+            return task;
+        }
     }
 
 }
