f15364bd0a89c3b0bcbbd0218380ad5169fb4d32
Yee <yee@fanruan.com>
2019-05-21 16:57:54 +0800

    Directory

---
+++ src/main/java/com/fineio/v3/file/DirectoryBlock.java
@@ -0,0 +1,49 @@
+package com.fineio.v3.file;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author yee
+ */
+public class DirectoryBlock implements Block {
+
+    /**
+     *
+     */
+    private String dirPath;
+    /**
+     *
+     */
+    private List<Block> files;
+
+    /**
+     * Default constructor
+     */
+    public DirectoryBlock(String dirPath, List<Block> blocks) {
+        this.dirPath = dirPath;
+        this.files = blocks;
+    }
+
+    /**
+     *
+     */
+    public List<Block> getFiles() {
+        return this.files;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public String getPath() {
+        return dirPath;
+    }
+
+    @Override
+    public String getName() {
+        return new File(dirPath).getName();
+    }
+
+}
