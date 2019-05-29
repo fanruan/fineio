9bd42c9cac14198268e292ce8c19098a97dca1df
Yee <yee@fanruan.com>
2019-05-29 19:53:02 +0800

    KERNEL-626 cache MemoryLimit

--- src/main/java/com/fineio/v3/file/impl/BufferCache.java
+++ src/main/java/com/fineio/v3/file/impl/BufferCache.java
@@ -5,6 +5,7 @@ import com.fineio.logger.FineIOLoggers;
 import com.fineio.v3.buffer.DirectBuffer;
 import com.fineio.v3.cache.core.Cache;
 import com.fineio.v3.cache.core.Caffeine;
+import com.fineio.v3.memory.MemoryManager;
 
 import java.text.MessageFormat;
 import java.util.concurrent.TimeUnit;
@@ -41,7 +42,7 @@ public class BufferCache {
     private BufferCache() {
         cache = Caffeine.newBuilder()
                 // TODO: 2019/5/28 anchore weight定为读内存上限，需要memory manager资瓷下
-                .maximumWeight(Runtime.getRuntime().maxMemory() >> 1)
+                .maximumWeight((MemoryManager.INSTANCE.getCacheMemoryLimit() >> 12) - 1)
                 .<FileBlock, DirectBuffer>weigher((key, value) -> value.getSizeInBytes())
                 .expireAfterAccess(60, TimeUnit.MINUTES)
                 .recordStats()
